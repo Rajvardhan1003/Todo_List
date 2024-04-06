702bb662a1da9643916349e184b9b2a4735dedf9
Rajvardhan1003 <m.rvardhan4882@gmail.com>
2024-04-06 23:30:21 +0530

    Initial committ

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button add;
+    AlertDialog dialog;
+    LinearLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        add=findViewById(R.id.add);
+        layout=findViewById(R.id.container);
+        buildDialog();
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dialog.show();
+            }
+        });
+    }
+    public void buildDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        View view=getLayoutInflater().inflate(R.layout.dialog,null);
+
+        final EditText name=view.findViewById(R.id.nameEdit);
+
+        builder.setView(view);
+        builder.setTitle("Task you want to do")
+                .setPositiveButton("SAVE", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which){
+                        addcard(name.getText().toString());
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        dialog = builder.create();
+    }
+    private void addcard(String name){
+        final View view = getLayoutInflater().inflate(R.layout.card,null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        Button delete = view.findViewById(R.id.delete);
+        nameView.setText(name);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+        layout.addView(view);
+    }
+}
